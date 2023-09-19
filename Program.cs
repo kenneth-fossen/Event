137bc09f988d9a6fda2dee39d2af955dda250fb1
Kenneth Fossen <kenneth.fossen@bouvet.no>
2023-09-19 12:14:43 +0200

    Lists events also

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.Json;
 
@@ -8,14 +9,9 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("CLI 4 event.bouvet.no");
 
 // Your config
-var eventId = "";
+
 var token = "";
 
-if (string.IsNullOrWhiteSpace(eventId))
-{
-    Console.WriteLine("EventID");
-    eventId = Console.ReadLine();
-}
 
 if (string.IsNullOrWhiteSpace(token))
 {
@@ -25,21 +21,19 @@ if (string.IsNullOrWhiteSpace(token))
 
 
 var httpClient = new HttpClient();
-
+const string div = "-----------------------------------------------------------";
 const string schema = "Bearer";
 const string applicationJson = "application/json";
 const string baseUrl = "https://event.bouvet.no";
 const string endpoint = $"{baseUrl}/graphql";
 
-var getParticipents = $$$"""
-                         {
-                           "operationName": "GetEventById",
-                           "variables": {
-                             "eventId": "{{{eventId}}}"
-                           },
-                           "query": "query GetEventById($eventId: String!) {\n  event(eventId: $eventId) {\n    id\n    participants {\n      name\n      comment\n      unit\n      acceptanceStatus\n      acceptanceStatusEnum\n      userId\n      photo\n      showAllergies\n      numberOfGuests\n      email\n      waitlistPosition\n      __typename\n    }\n    __typename\n  }\n}\n"
-                         }
-                         """;
+var getEvents = $$$"""
+                {
+                  "operationName": "GetInvitedEvents",
+                  "variables": {},
+                  "query": "query GetInvitedEvents {\n  invitedEvents {\n    id\n    eventName\n    description\n    location\n    summary\n    startDate\n    endDate\n    responseDeadline\n    minParticipants\n    maxParticipants\n    numberOfGuestsAllowed\n    requireResponse\n    attendenceCount\n    isSocial\n    ownerName\n    collaborators\n    __typename\n  }\n}\n"
+                }
+""";
 
 var serializeOptions = new JsonSerializerOptions
 {
@@ -49,6 +43,47 @@ var serializeOptions = new JsonSerializerOptions
 
 httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(schema, token);
 httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(applicationJson));
+
+
+var eventResponse = await httpClient.PostAsync(endpoint, new StringContent(getEvents, Encoding.UTF8, applicationJson));
+eventResponse.EnsureSuccessStatusCode();
+var eventListResponseContent = await eventResponse.Content.ReadAsStringAsync();
+var eventListSelector = new Dictionary<int, string>();
+try
+{
+    var eventList = JsonSerializer.Deserialize<GrapQlReponse>(eventListResponseContent, serializeOptions);
+
+    Console.WriteLine(div);
+    foreach (var (invitedEvent, idx) in eventList.Data.InvitedEvents.Select((events, i) => (events, i)))
+    {
+        Console.WriteLine($"{idx} \t | {invitedEvent.Id} \t | {invitedEvent.EventName}");
+        eventListSelector.Add(idx, invitedEvent.Id);
+    }
+    Console.WriteLine(div);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+    throw;
+}
+
+
+Console.WriteLine("Select the ID (int) for the Event:");
+var selectedEventInt = Console.ReadLine();
+
+eventListSelector.TryGetValue(int.Parse(selectedEventInt), out var selectedEvent );
+
+var getParticipents = $$$"""
+                         {
+                           "operationName": "GetEventById",
+                           "variables": {
+                             "eventId": "{{{selectedEvent}}}"
+                           },
+                           "query": "query GetEventById($eventId: String!) {\n  event(eventId: $eventId) {\n    id\n    participants {\n      name\n      comment\n      unit\n      acceptanceStatus\n      acceptanceStatusEnum\n      userId\n      photo\n      showAllergies\n      numberOfGuests\n      email\n      waitlistPosition\n      __typename\n    }\n    __typename\n  }\n}\n"
+                         }
+                         """;
+
+
 var response = await httpClient.PostAsync(endpoint, new StringContent(getParticipents, Encoding.UTF8, applicationJson));
 
 Console.WriteLine($"HTTP Response Status: {response.StatusCode}");
@@ -63,7 +98,7 @@ if (response.IsSuccessStatusCode)
         var entity = JsonSerializer.Deserialize<GrapQlReponse>(content, serializeOptions);
         Console.WriteLine();
         var count = entity.Data.Event.Participants.Count(p => p.AcceptanceStatus == "GOING");
-        const string div = "-----------------------------------------------------------";
+
         Console.WriteLine(div);
         Console.WriteLine($"Attending: {entity.Data.Event.Id}: No: {count}");
         Console.WriteLine(div);
@@ -103,7 +138,11 @@ else
 
 internal record GrapQlReponse(Data Data);
 
-internal record Data(Event Event);
+internal class Data {
+    public IList<InvitedEvents> InvitedEvents { get; set; }
+    public Event Event { get; set; }
+}
+
 
 internal record Event(string Id, IList<Participant> Participants);
 
@@ -116,4 +155,13 @@ internal record Participant
     public string? UserId { get; set; }
     public string? Photo { get; set; }
     public bool? ShowAllergies { get; set; }
+}
+
+internal class InvitedEvents
+{
+    public string Id { get; set; }
+    public string EventName { get; set; }
+    public string Owner { get; set; }
+    public string StartDate { get; set; }
+    public string EndDate { get; set; }
 }
